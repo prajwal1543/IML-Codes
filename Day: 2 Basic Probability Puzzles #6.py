f0ece1b257a4e380f0996638dadb830162b676e1
prajwal1543 <ipcworshipmusic123@gmail.com>
2024-07-20 01:26:50 +0530

    Create Day: 2 Basic Probability Puzzles #6.py

---
+++ Day: 2 Basic Probability Puzzles #6.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from fractions import Fraction
+
+x = ['w']*5 + ['b']*4
+y = ['w']*7 + ['b']*6
+
+def black_probability(x,y):
+    return (Fraction(x.count('w') * y.count('b'), len(x) * (len(y)+1)) 
+            + 
+            Fraction(x.count('b') * (y.count('b')+1), len(x) * (len(y)+1)))
+    
+print(black_probability(x,y))
